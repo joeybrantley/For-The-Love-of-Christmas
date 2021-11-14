7d2cac5c476e2cbf931db4cb051a8338828003fa
Joey Brantley <oddsome@gmail.com>
2021-11-13 16:52:44 -0800

    Added JavaScript elements to web page

---
+++ apps.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const button = document.querySelector('button');
+const paragraph = document.querySelector('p');
+
+button.addEventListener('click', updateButton);
+
+function updateButton() {
+  if (button.value === 'What is the best Christmas movie?') {
+    button.value = 'What is the best Christmas movie?';
+    paragraph.textContent = 'Also Christmas Vacation.';
+  }
+  else {
+    button.value = 'What is the best Christmas movie?';
+    paragraph.textContent = 'Christmas Vacation.';
+  }
+}
+
+
+let userName = prompt("What is your name?");
+
+let currentHour = prompt("What is the current hour of the day (1-24)");
+let greeting = "";
+
+if(currentHour >= 17 && currentHour <= 24){
+    greeting = "Good Evening"; 
+} else if(currentHour >= 12 && currentHour <= 16){
+    greeting = "Good Afternoon";
+} else if(currentHour > 0 ) {
+    greeting = "Good Morning";
+}else {
+    greeting = "Welcome";
+}
+
+document.write(greeting + ' , ' + userName.toUpperCase() + "!");
