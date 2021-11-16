c1bde7a3ddae9cf68a3a383b30c13f7c17df4015
Joey Brantley <oddsome@gmail.com>
2021-11-15 20:35:22 -0800

    Cleaned up JS code by converting to functions

--- apps.js
+++ apps.js
@@ -16,22 +16,34 @@ function updateButton() {
   }
 }
 
+
 button.addEventListener('click', updateButton);
 
 
-let userName = prompt("What is your name?");
-
-let currentHour = prompt("What is the current hour of the day (1-24)");
-let greeting = "";
-
-if(currentHour >= 17 && currentHour <= 24){
-    greeting = "Good Evening"; 
-} else if(currentHour >= 12 && currentHour <= 16){
-    greeting = "Good Afternoon";
-} else if(currentHour > 0 ) {
-    greeting = "Good Morning";
-}else {
-    greeting = "Welcome";
-}
-
-document.write(greeting + ' , ' + userName.toUpperCase() + "!");
+function createGreeting(){
+  let userName = getUserName();
+  let greeting = hourOfDay();
+  document.write(greeting + ',' + userName + "!");
+ }
+ 
+ function getUserName(){
+     let userName = prompt("What is your name?"); // AmAndA >> amanda 
+     return userName;
+ }
+ 
+ function hourOfDay(){
+     let currentHour = prompt("What is the current hour of the day (1-24)");
+     let greeting = "";
+     if(currentHour >= 17 && currentHour <= 24){
+         greeting = "Good Evening"; 
+     } else if(currentHour >= 12 && currentHour <= 16){
+         greeting = "Good Afternoon";
+     } else if(currentHour > 0 ) {
+         greeting = "Good Morning";
+     }else {
+         greeting = "Welcome";
+     }
+ 
+     return greeting;
+ }
+ 
